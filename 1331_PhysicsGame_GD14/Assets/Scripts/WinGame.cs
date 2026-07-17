812c84fdd5bc8618acd8cf272091fcc569a1364e
Epicjeong <yurijeong@icloud.com>
2026-07-16 21:27:31 -0700

    final adjustments

--- 1331_PhysicsGame_GD14/Assets/Scripts/WinGame.cs
+++ 1331_PhysicsGame_GD14/Assets/Scripts/WinGame.cs
@@ -6,10 +6,12 @@ public class WinGame : MonoBehaviour
     [SerializeField] private GameObject _winScreen;
     [SerializeField] private Timer _timer;
     [SerializeField] private TextMeshProUGUI _finalTimeText;
+    [SerializeField] private AudioSource _winSound;
 
     private void OnTriggerEnter(Collider other)
     {
         _finalTimeText.text = "" + _timer._time;
+        _winSound.Play();
         _winScreen.SetActive(true);
     }
 }
